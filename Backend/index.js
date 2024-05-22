9a876d7ce87cf2c92979b87c89d13e94edb59e53
João Pedro <joaoofficialpedro@gmail.com>
2024-05-22 20:59:47 -0300

    Feat: Added temporary data generator for plants

--- Backend/index.js
+++ Backend/index.js
@@ -6,5 +6,32 @@ const { server, plugins } = require("./server");
     await server.start()
     console.log("Server listening: " + server.info.uri);
 
-    // server.logger.info(`Server listening: ${server.info.uri}`)
+    generateDataToPlants()
 })()
+
+function generateDataToPlants() {
+    const cron = require('node-cron')
+    const plants = require('./src/plants/plants-repository')
+    const dataPlants = require('./src/data_plants/data-plants-repository')
+
+    cron.schedule('*/10 * * * *', async () => {
+        const allPlants = await plants.findAll({})
+        console.log(`Total de Plantas: ${allPlants.length}`)
+
+        const genRandom = () => {
+            return Math.floor(Math.random() * 101);
+        }
+
+        allPlants.forEach(async plant => {
+            const object = {
+                plant_id: plant.id,
+                illumination: genRandom(),
+                celsius: genRandom(),
+                humidity: genRandom(),
+                weight: genRandom() * 10
+            }
+            await dataPlants.create(object)
+            console.log(`Planta ${plant.name}: ${JSON.stringify(object)}`)
+        })
+    })
+}
